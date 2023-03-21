be231e4a53d982f5c285df29b381a4b4b6c1a696
Wilson Oliveira Junior <Wilsono.junior@outlook.com>
2023-03-21 11:06:55 -0300

    done

--- app/Assets/Js/Script.js
+++ app/Assets/Js/Script.js
@@ -34,3 +34,17 @@ $('.play').on('click',function(){
 $('.stop').on('click',function(){
     owl.trigger('stop.owl.autoplay')
 })
+
+console.clear();
+const featuresEl = document.querySelector(".features");
+const featureEls = document.querySelectorAll(".feature");
+
+featuresEl.addEventListener("pointermove", (ev) => {
+  featureEls.forEach((featureEl) => {
+    // Not optimized yet, I know
+    const rect = featureEl.getBoundingClientRect();
+
+    featureEl.style.setProperty("--x", ev.clientX - rect.left);
+    featureEl.style.setProperty("--y", ev.clientY - rect.top);
+  });
+});
