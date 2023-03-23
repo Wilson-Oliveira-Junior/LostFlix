45c4423603d0541ae199d5c2f1f02a63a523e9ce
Wilson Oliveira Junior <Wilsono.junior@outlook.com>
2023-03-23 15:10:06 -0300

    done

--- app/Assets/Js/Script.js
+++ app/Assets/Js/Script.js
@@ -1,38 +1,38 @@
-$(document).ready(function(){
-    $(".custom-carousel").owlCarousel({
+$(document).ready(function () {
+  $(".custom-carousel").owlCarousel({
     autoWidth: true,
     loop: true,
-    autoplay:true,
-    autoplayHoverPause:true,
-    responsive:{
-        0:{
-          items:1,
-          loop:true
-        },
-        600:{
-          items:3,
-          loop:true
-        },
-        1000:{
-          items:4,
-          loop:true
-        }
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        loop: true
+      },
+      600: {
+        items: 3,
+        loop: true
+      },
+      1000: {
+        items: 4,
+        loop: true
       }
+    }
   });
 });
-  
+
 
 $(document).ready(function () {
-    $(".custom-carousel .item").click(function () {
-        $(".custom-carousel .item").not($(this)).removeClass("active");
-        $(this).toggleClass("active");
-    });
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+  });
 });
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[5000])
+$('.play').on('click', function () {
+  owl.trigger('play.owl.autoplay', [5000])
 })
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
+$('.stop').on('click', function () {
+  owl.trigger('stop.owl.autoplay')
 })
 
 console.clear();
